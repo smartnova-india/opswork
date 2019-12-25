431a5e9e8d34eecea0b13548ddf3bed915da4abc
Rajesh Soni <rajesh.soni@eeziklik.com>
2019-12-25 21:26:28 +0530

    first commit

---
+++ cookbooks/example/recipes/default.rb
@@ -0,0 +1,12 @@
+# This is a Chef Infra Client recipe file. It can be used to specify resources
+# which will apply configuration to a server.
+
+log "Welcome to Chef Infra Client, #{node['example']['name']}!" do
+  level :info
+end
+
+# For more information, see the documentation: https://docs.chef.io/recipes.html
+# 
+file '/var/www/html/phpinfo.php'
+content '<?php phpinfo();?>'
+end
